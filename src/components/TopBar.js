5eae14615a1261d1ef4793d579314dca33d1a7aa
numbafye <saadiqgp@gmail.com>
2023-08-13 16:50:50 -0400

    finished component pages but bootstrap styling isn't working

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,12 +2,12 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 
-export default function TopBar() {
+function TopBar() {
   return (
     <div>
       <Navbar bg="light" expand="lg">
         <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
-          <img src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" />{" "}
+          <img src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -33,3 +33,5 @@ export default function TopBar() {
     </div>
   );
 }
+
+export default TopBar;
